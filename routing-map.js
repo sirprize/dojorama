a4c3b226471944073a5905b47cf79ab9c2119ffc
sirprize <hoegl@backdoor.ch>
2012-12-26 21:19:03 +0100

    Make layer configuration optional

--- routing-map.js
+++ routing-map.js
@@ -2,7 +2,7 @@ define(["dojo/_base/config", "require"], function (config, require) {
     "use strict";
     
     var p = config['routing-map'].pathPrefix,
-        l = config['routing-map'].layers,
+        l = config['routing-map'].layers || {},
         mid = require.toAbsMid
     ;
     
