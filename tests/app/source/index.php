dcd524a6d2dd294da6cf20bc71989429054c7459
sirprize <hoegl@backdoor.ch>
2013-01-01 09:10:27 +0100

    Add missing <meta="viewport"...>

--- tests/app/source/index.php
+++ tests/app/source/index.php
@@ -7,6 +7,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php echo $base; ?>/styles/global.css">
         <title>Browse Source</title>
         <script>
